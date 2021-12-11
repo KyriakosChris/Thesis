5a506b20309c3f7d050ca8491c9a95ed2c0bde38
Kyriakos Christodoulidis <kchristodoulidis@isc.tuc.gr>
2021-12-11 18:37:55 +0200

    NN

--- Code/DatasetCode/DatasetModifier/loadDataset.py
+++ Code/DatasetCode/DatasetModifier/loadDataset.py
@@ -1,36 +1,53 @@
-import read_bvh
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Dense,MaxPooling1D,Softmax, LSTM, Dropout
+from tensorflow.keras import utils
 import numpy as np
-from os import listdir
-import os
+import pickle
+import glob
+import pandas as pd
+from Read_AllFiles import ReadFiles
+from numpy import load
+from tqdm import tqdm
 
 
-def generate_traindata_from_bvh(src_bvh_folder, tar_traindata_folder):
-    print ("Generating training data for "+ src_bvh_folder)
-    if (os.path.exists(tar_traindata_folder)==False):
-        os.makedirs(tar_traindata_folder)
-    bvh_dances_names=listdir(src_bvh_folder)
-    for bvh_dance_name in bvh_dances_names:
-        name_len=len(bvh_dance_name)
-        if(name_len>4):
-            if(bvh_dance_name[name_len-4: name_len]==".bvh"):
-                print ("Processing "+bvh_dance_name)
-                dance=read_bvh.get_train_data(src_bvh_folder+bvh_dance_name)
-                np.save(tar_traindata_folder+bvh_dance_name+".npy", dance)
 
-def generate_bvh_from_traindata(src_train_folder, tar_bvh_folder):
+
+path = ('/home/kyriakos/Desktop/Projects/Dataset/CMU_nmpy')
+files=ReadFiles.getListOfFiles(None,path)
+x  = []
+y = []
+seq = []
+seq_len = 100
+for file in tqdm(files):
+    x.append(np.load(file))
+
     
-    print ("Generating bvh data for "+ src_train_folder)
-    if (os.path.exists(tar_bvh_folder)==False):
-        os.makedirs(tar_bvh_folder)
-    dances_names=listdir(src_train_folder)
-    for dance_name in dances_names:
-        name_len=len(dance_name)
-        if(name_len>4):
-            if(dance_name[name_len-4: name_len]==".npy"):
-                print ("Processing"+dance_name)
-                dance=np.load(src_train_folder+dance_name)
-                dance2=[]
-                for i in range(dance.shape[0]/8):
-                    dance2=dance2+[dance[i*8]]
-                print (len(dance2))
-                read_bvh.write_traindata_to_bvh(tar_bvh_folder+dance_name+".bvh",np.array(dance2))
+
+X= np.asarray(x, dtype=object)
+###   (4320,150,13,2) ---->>>   (4320, 150, 26)
+
+#X = np.reshape(X, (X.shape[0], X.shape[1],X.shape[2]*X.shape[3]))
+#X= X/np.max(X)
+print(X.shape)
+
+#y = utils.to_categorical(y)
+
+
+model = Sequential()
+model.add(LSTM(256, input_shape = (X.shape[0]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
+model.add(Dropout(0.2))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-biggeer.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose = 1, save_best_only=True, mode = 'min')
+callbacks_list = [checkpoint]
+
+model.fit(X, y, epochs = 30, batch_size=32, callbacks=callbacks_list)
