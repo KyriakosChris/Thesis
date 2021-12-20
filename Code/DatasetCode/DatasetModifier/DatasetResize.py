1f815d8b01b202ad3edc7e1d668e3160ae2c004a
Kyriakos Christodoulidis <kchristodoulidis@isc.tuc.gr>
2021-12-20 06:50:44 +0200

    FirstNN

--- Code/DatasetCode/DatasetModifier/DatasetResize.py
+++ Code/DatasetCode/DatasetModifier/DatasetResize.py
@@ -5,12 +5,14 @@ from numpy.lib.function_base import average
 import pandas as pd
 from Read_AllFiles import ReadFiles
 from numpy import load
+from numpy import save
 from tqdm import tqdm
 
 
 
 
-path = "D:\\tuc\\exam10\\Thesis\\Dataset\\nmpy_Resized\\Sample"
+path = "D:\\tuc\\exam10\\Thesis\\Dataset\\CMU_nmpy"
+out = "D:\\tuc\\exam10\\Thesis\\Dataset\\nmpy_Resized\\"
 files=ReadFiles.getListOfFiles(None,path)
 shape = []
 for file in tqdm(files):
@@ -20,9 +22,24 @@ print(max(shape))
 print(min(shape))
 print(average(shape))
 m = max(shape)
+
+for file in tqdm(files):
+    data = load(file, allow_pickle=False)
+    d = data[data.files[0]]
+    f= np.resize(d,(m,d.shape[1],d.shape[2]))
+    name= out +file.split("\\")[6].split(".npz")[0]
+    np.savez_compressed(name, f)
+
+
+path = "D:\\tuc\\exam10\\Thesis\\Dataset\\nmpy_Resized"
+
+files=ReadFiles.getListOfFiles(None,path)
+clips = []
 for file in tqdm(files):
     data = load(file, allow_pickle=True)
-    d = data[data.files[0]]
-    data = data.reshape((data.shape[0], data.shape[1], 1))
-    
-    break
+    clips.append(data[data.files[0]])
+
+name = "D:\\tuc\\exam10\\Thesis\\Dataset\\CMU_clips"
+merged = np.asarray(clips)
+print(merged.shape)
+np.savez_compressed(name, merged)
---
+++ Code/NeuralNetworkCode/Classifier.py
@@ -0,0 +1,88 @@
+#pyPi libraries
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import *
+from tensorflow.keras import utils
+from keras.optimizers import Adam
+from numpy import load
+import numpy as np
+from numpy import argmax
+from pandas import read_csv
+from keras.models import load_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+#This configures GPUs so that memory issues don't occur
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+# Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        raise e
+
+
+path = "D:\\tuc\\exam10\\Thesis\\Dataset\\CMU_clips_no_zip.npy"
+
+X = load(path, allow_pickle=True)
+print(X.shape)
+y = []
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.1)
+nNeurons = 186
+
+# creating the NN model
+model = Sequential()
+model.add( LSTM( nNeurons, return_sequences = True, input_shape = X.shape ) )
+model.add( LSTM( nNeurons ) )
+model.add( Dense( 3, activation = 'softmax' ) )
+model.compile( loss = 'mse', optimizer = Adam( lr = 0.001 ), metrics = ['accuracy'] )
+model.summary()
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-biggeer.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose = 1, save_best_only=True, mode = 'min')
+callbacks_list = [checkpoint]
+
+model.fit(X_train, y_train, epochs = 30, batch_size=32, callbacks=callbacks_list)
+model.save('cmuclassifier.model')
+
+y_pred = model.predict(X_test)
+#Converting predictions to label
+pred = list()
+for i in range(len(y_pred)):
+    pred.append(np.argmax(y_pred[i]))
+#Converting one hot encoded test label to label
+test = list()
+for i in range(len(y_test)):
+    test.append(np.argmax(y_test[i]))
+
+
+a = accuracy_score(pred,test)
+print('Accuracy is:', a*100)
+
+history = model.fit(X_train, y_train,validation_data = (X_test,y_test), epochs=100, batch_size=64)
+
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss']) 
+plt.plot(history.history['val_loss']) 
+plt.title('Model loss') 
+plt.ylabel('Loss') 
+plt.xlabel('Epoch') 
+plt.legend(['Train', 'Test'], loc='upper left') 
+plt.show()
