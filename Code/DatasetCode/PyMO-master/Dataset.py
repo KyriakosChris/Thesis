8aead95d44a0f8db33abf623292a981dd336e3ff
Kyriakos Christodoulidis <kchristodoulidis@isc.tuc.gr>
2022-03-24 01:24:26 +0200

    Delete Code/DatasetCode directory

--- Code/DatasetCode/PyMO-master/Dataset.py
+++
@@ -1,5 +0,0 @@
-from pymo.parsers import BVHParser
-
-parser = BVHParser()
-
-parsed_data = parser.parse('D:\\tuc\\exam10\\Thesis\\Dataset\\07_06.bvh')
--- Code/DatasetCode/PyMO-master/bvh-file-parser-python-3-master/main.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-## bvh parser sample script
-import bvh_parser as bp
-
-if __name__ == "__main__":
-	# parse bvh file
-	bvh_parser = bp.bvh("D:\\tuc\\exam10\\Thesis\\Dataset\\07_06.bvh")
-	# print first frame motion data
-	print(bvh_parser.motions[0])
