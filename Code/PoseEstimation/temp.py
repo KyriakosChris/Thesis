eb8200f6ca790181752f6d02989f7a4e3a954dae
Kyriakos Christodoulidis <kchristodoulidis@isc.tuc.gr>
2022-05-03 19:55:22 +0300

    report init

--- Code/PoseEstimation/temp.py
+++ Code/PoseEstimation/temp.py
@@ -1,60 +1,26 @@
-
-import tkinter as tk
 from tkinter import *
-import cv2
-from PIL import Image, ImageTk
-import os
-import numpy as np
 
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
+   
+root = Tk()
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open File", command=donothing)
+filemenu.add_command(label="Save File", command=donothing)
+filemenu.add_command(label="Close", command=donothing)
 
-global last_frame                                      #creating global              variable
-last_frame = np.zeros((480, 640, 3), dtype=np.uint8)
-global last_frame2                                      #creating global      variable
-last_frame2 = np.zeros((480, 640, 3), dtype=np.uint8)
-global cap
-cap = cv2.VideoCapture(1)
+filemenu.add_separator()
 
-def show_vid():                                        #creating a function
-    if not cap.isOpened():                             #checks for the opening of camera
-        print("cant open the camera")
-        flag, frame = cap.read()
-        frame = cv2.resize(frame,(400,500))
-        if flag is None:
-            print ("Major error!")
-    elif flag:
-        global last_frame
-        last_frame = frame.copy()
-        global last_frame2
-        last_frame2 = frame.copy()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
 
-pic = cv2.cvtColor(last_frame, cv2.COLOR_BGR2RGB)     #we can change the display color of the frame gray,black&white here
-img = Image.fromarray(pic)
-imgtk = ImageTk.PhotoImage(image=img)
-# lmain.imgtk = imgtk
-# lmain.configure(image=imgtk)
-# lmain.after(10, show_vid)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
 
-
-def show_vid2():
-    pic2 = cv2.cvtColor(last_frame2, cv2.COLOR_BGR2GRAY)
-    img2 = Image.fromarray(pic2)
-    img2tk = ImageTk.PhotoImage(image=img2)
-    lmain2.img2tk = img2tk
-    lmain2.configure(image=img2tk)
-    lmain2.after(10, show_vid2)
-
-if __name__ == '__main__':
-    root=tk.Tk()                                     #assigning root variable        for Tkinter as tk
-    lmain = tk.Label(master=root)
-    lmain2 = tk.Label(master=root)
-    #lmain.Frame= Frame(width=768, height=576)
-    #framex.grid(column=3,rowspan=2,padx=5, pady=5)
-    lmain.pack(side = LEFT)
-    lmain2.pack(side = RIGHT)
-    root.title("Fire Alarm Detector")            #you can give any title
-    root.geometry("900x700+100+10") #size of window , x-axis, yaxis
-    exitbutton = Button(root, text='Quit',fg="red",command=   root.destroy).pack(side = BOTTOM,)
-    show_vid()
-    show_vid2()
-    root.mainloop()                                  #keeps the application in an infinite loop so it works continuosly
-    cap.release()
+root.config(menu=menubar)
+root.mainloop()
